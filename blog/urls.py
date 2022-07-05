87b47f20bee2bec32ed2206695393ba01dcd498e
Yujin_nKim <yujin.kimd@gmail.com>
2022-07-05 13:27:39 +0900

    create blog post menu page using CBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views #같은 폴더 내에 views.py import함
 
 urlpatterns = [
-    path('', views.index), #~blog/ 뒤에 아무것도 없을때
+    # path('', views.index), #~blog/ 뒤에 아무것도 없을때
     path('<int:pk>/', views.single_post_page),
+
+    path('', views.PostList.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from .models import Post
 
-# Create your views here.
+from django.views.generic import ListView
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# django에서 제공하는 ListView 상속받아서 사용
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html' #이부분 없애고 html을 post_list.html로 바꿔도 됨
+    #템플릿에서 post_list로 사용해야함
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+    #list로 쭉 보여주는 것을 사용하고 싶다면 모델명막 적어줘도 된다.
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
